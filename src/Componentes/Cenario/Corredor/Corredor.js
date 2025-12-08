c33ddb235d32af3eee05df1c3573911cfeaecc4e
diogokoizumi <diogokoizumi3@gmail.com>
2025-12-07 23:10:23 -0300

    Update Corredor.js

--- src/Componentes/Cenario/Corredor/Corredor.js
+++ src/Componentes/Cenario/Corredor/Corredor.js
@@ -1,15 +1,16 @@
 function addCorridor(startZ, endZ) {
-    // Piso
-    quad([-3.0,0,startZ],[3.0,0,startZ],[3.0,0,endZ],[-3.0,0,endZ],[0.82,0.82,0.75]);
-    // Teto
-    quad([-3.0,6,startZ],[3.0,6,startZ],[3.0,6,endZ],[-3.0,6,endZ],[0.9,0.88,0.85]);
-    // Paredes
-    quad([-3.0,0,startZ],[-3.0,6,startZ],[-3.0,6,endZ],[-3.0,0,endZ],[0.95,0.9,0.85]);
-    quad([3.0,0,startZ],[3.0,6,startZ],[3.0,6,endZ],[3.0,0,endZ],[0.95,0.9,0.85]);
+    // Piso -> TRUE (Aplica textura repetida)
+    quad([-3.0,0,startZ],[3.0,0,startZ],[3.0,0,endZ],[-3.0,0,endZ],[0.82,0.82,0.75], true);
 
-    // Start Line at Z = 0
+    // Teto -> FALSE (Cor sólida)
+    quad([-3.0,6,startZ],[3.0,6,startZ],[3.0,6,endZ],[-3.0,6,endZ],[0.9,0.88,0.85], false);
+
+    // Paredes -> FALSE (Cor sólida)
+    quad([-3.0,0,startZ],[-3.0,6,startZ],[-3.0,6,endZ],[-3.0,0,endZ],[0.95,0.9,0.85], false);
+    quad([3.0,0,startZ],[3.0,6,startZ],[3.0,6,endZ],[3.0,0,endZ],[0.95,0.9,0.85], false);
+
+    // Start Line -> FALSE
     if (0 <= startZ && 0 >= endZ) {
-        // White strip
-        quad([-3.0, 0.01, 0.5], [3.0, 0.01, 0.5], [3.0, 0.01, -0.5], [-3.0, 0.01, -0.5], [1, 1, 1]);
+        quad([-3.0, 0.01, 0.5], [3.0, 0.01, 0.5], [3.0, 0.01, -0.5], [-3.0, 0.01, -0.5], [1, 1, 1], false);
     }
 }
